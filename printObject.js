ae0de4e7e555b68f77141042c9131f50bf26124d
JoeJenson <joe.j.jenson+github@gmail.com>
2016-02-16 17:54:37 -0500

    Update printObject.js to use correct tabbing

--- printObject.js
+++ printObject.js
@@ -7,22 +7,22 @@ var printObject = function(object, tabbing) {
       printed = '\n';
       if (object instanceof Array) {
         for (var i = 0; i < object.length; i++) {
-          printed += printObject(object[i], tabbing + 1) + '\n';
+          printed += (new Array(tabbing + 1)).join('  ') + printObject(object[i], tabbing + 1) + '\n';
         }
       }
       else {
         for (var i in object) {
           if (object.hasOwnProperty(i)) {
-            printed += printObject(i, tabbing + 1) + ': ' + printObject(object[i], tabbing + 1) + '\n';
+            printed += (new Array(tabbing + 1)).join('  ') + printObject(i, 0) + ': ' + printObject(object[i], tabbing + 1) + '\n';
           }
         }
       }
       break;
     case 'string':
-      printed = '"' + object.replace(/([^\\])"/g, '$1\\"') + '"';
+      printed = '"' + object.replace(/([^\\])"/g, '$1\\"').replace('\n', '\\n').replace('\\', '\\\\').replace('\t', '\\t') + '"';
       break;
     default:
       printed = object.toString();
   }
-  return (new Array(tabbing + 1)).join('  ') + printed;
+  return printed;
 };
